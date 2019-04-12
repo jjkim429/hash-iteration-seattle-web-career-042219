e79b085a09871b6414184047fbfeca5ee5fa685f
Jennifer Kim <jjkim429@gmail.com>
2019-04-12 01:20:46 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,8 +5,9 @@
 # }
 
 def happy_birthday(birthday_kids)
-  birthday_kids.each do |kid, age|
-    puts "You are now #{age} years old!"
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
 end
 
 
