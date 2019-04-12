ed05b30f3b46754eb064fd85c0834de17b6b0947
Jennifer Kim <jjkim429@gmail.com>
2019-04-12 01:23:42 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,5 +12,9 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    if suite == :suite_a && name.start_with?("A")
+    if age < 12
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    end
+  end
+end
 
